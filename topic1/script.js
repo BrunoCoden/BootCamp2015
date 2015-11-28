8634c82623a78f5fecd82f94ceb291917b615d75
BruNazo <coden.brunoa@gmail.com>
2015-11-27 22:54:55 -0300

    Topic 1 terminado

--- topic1/script.js
+++ topic1/script.js
@@ -1,9 +1,48 @@
-$(document).ready(function(){
-	$(".textoOculto").show("1200", function(){
+$(document).on("ready",function(){
+	$(".textoOculto").show("300", function(){
 		$(".alias").focus();
 	});
+	$("#respuesta").on("click",function() {
+		var nombre = $("#nombre");
+		$.ajax("http://bootcamp.aws.af.cm/welcome/Bruno")
+			.done(function(data){
+				$(".welcome").html(data.response);
+
+			})
+			.fail(function(data){
+				$(".welcome").css({
+					"background-color":"red", 
+					"height":"40px"
+				});
+			});
+	});
+		$("#botonDeBusqueda").on("click",function(){
+			ejecutar_busqueda();
+		});
+		function ejecutar_busqueda (){
+			var busqueda = prompt("Please search your artist");
+			$.ajax("https://api.spotify.com/v1/search?q="+busqueda+"&type=album")
+			.done(function(data){
+			
+			$.each(data.albums.items, function(ix, value) {
+				$("#article").append("<img src="+value.images[0].url+" />");
+				$("#article").append("Album: "+value.name+"</br>");
+				$("#article").append("Type: "+value.type+"</br>");
+				$.ajax({
+					url:value.href,
+					async:false
+				}).done(function(valor){
+					$("#article").append("Release date: "+valor.release_date+"</br>");
+				})
+				$("#article").append("<a href="+value.external_urls.spotify+">Ir al Album</a></br>");
+			})	
+			})
+			.fail(function(data){
+				alert("error");
+			})	
+		}
+	$("#buttonReloadIndex").on("click",function(){
+		location.reload();
+	})
 
 })
-
-
-
